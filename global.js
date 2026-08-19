f244f188ae2363894552f4903a11acedb9f65008
Susikumar <susikumar0123@gmail.com>
2026-08-19 12:54:01 +0530

    Updated localhost to API_BASE_URL for Render

--- global.js
+++ global.js
@@ -1,3 +1,7 @@
+const API_BASE_URL = window.location.hostname === 'localhost' 
+    ? 'http://localhost:3000' 
+    : 'https://catus-backend-d2js.onrender.com';
+
 // ==========================================
 // 1. GLOBAL VARIABLES
 // ==========================================
